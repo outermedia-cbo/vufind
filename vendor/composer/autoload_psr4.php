63cbce12bca8c38ea85453caffd2b2276c4203dd
Guenter Hipler <guenter.hipler@unibas.ch>
2015-05-22 11:23:00 +0200

    add additional dependencies

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'Elasticsearch\\' => array($vendorDir . '/elasticsearch/elasticsearch/src/Elasticsearch'),
     'Behat\\Mink\\' => array($vendorDir . '/behat/mink/src'),
 );
