c2d01bf01b4100d3e103775e1b4282000bd82533
Guenter Hipler <guenter.hipler@unibas.ch>
2015-06-23 23:13:20 +0200

    start to fetch single values out of search-engine fields

--- module/LinkedSwissbib/src/LinkedSwissbib/RecordDriver/ElasticSearchRDF.php
+++ module/LinkedSwissbib/src/LinkedSwissbib/RecordDriver/ElasticSearchRDF.php
@@ -13,6 +13,25 @@ use VuFind\RecordDriver\AbstractBase;
 
 class ElasticSearchRDF extends AbstractBase {
 
+    protected $propertiesToNameSpaces = [
+        'rdf:type'  => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+        'dc11:format'   => 'http://purl.org/dc/elements/1.1/format',
+        'dc:hasPart'    => 'http://purl.org/dc/terms/hasPart',
+        'dc:issued' =>  'http://purl.org/dc/terms/issued',
+        'dc:language'   =>  'http://purl.org/dc/terms/language',
+        'dc:title'  =>  'http://purl.org/dc/terms/title',
+        'bibo:isbn10'   =>  'http://purl.org/ontology/bibo/isbn10',
+        'bibo:isbn13'   =>  'http://purl.org/ontology/bibo/isbn13',
+        'ns0:contentType'   =>  'http://rdaregistry.info/Elements/u/contentType',
+        'ns0:mediaType' =>  'http://rdaregistry.info/Elements/u/mediaType',
+        'ns0:noteOnResource'    =>  'http://rdaregistry.info/Elements/u/noteOnResource',
+        'ns0:placeOfPublication'    =>  'http://rdaregistry.info/Elements/u/placeOfPublication',
+        'ns0:publicationStatement'  =>  'http://rdaregistry.info/Elements/u/publicationStatement',
+        'rdfs:isDefinedBy'  =>  'http://www.w3.org/2000/01/rdf-schema#isDefinedBy'
+
+    ];
+
+
     /**
      * Get text that can be displayed to represent this record in breadcrumbs.
      *
@@ -31,59 +50,22 @@ class ElasticSearchRDF extends AbstractBase {
      */
     public function getUniqueID()
     {
-        throw new \Exception(__FUNCTION__ . ' currently not supported');
+        return $this->fields['_source']['@id'];
     }
 
 
-    public function getRDF($rdfType = 'turtle')
+    public function getRdf($rdfType = 'turtle')
     {
 
-        $source = $this->fields['_source'];
-        $rdfJson = json_encode($source);
         $rdfGraph = new \EasyRdf_Graph();
-        $result = $rdfGraph->parse($rdfJson,'jsonld');
-
-
-
-        //foreach ($rdfGraph as $subject) {
-        //    $test = "";
-        //}
-        $myResource = $rdfGraph->resources();
-        $php = $rdfGraph->toRdfPhp();
-        $turtle = $rdfGraph->serialise($rdfType);
-        //$graph2 = new \EasyRdf_Graph();
-        //$graph2->parse($turtle,'turtle');
-
-        //$language = $graph2->getResource('http://data.swissbib.ch/resource/212817795','dc:language');
-
-        //$properties = $graph2->properties('http://data.swissbib.ch/resource/212817795');
-        //foreach($properties as $property) {
-
-        //    $wasistDas = $graph2->get('http://data.swissbib.ch/resource/212817795',$property);
-        //    $dd = "";
-
-        //}
-        $citation = $rdfGraph->getLiteral('http://data.swissbib.ch/resource/212817795','dc:bibliographicCitation');
-        $languageFromGraph = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','dc:language');
-        //$uri = $rdfGraph->allResources('http://data.swissbib.ch/resource/212817795','dc:bibliographicCitation');
-        //return $rdfGraph->dump();
-        //$hello = \EasyRdf_Graph::newAndLoad("http://www.dbpedialite.org/things/10");
-        //$AA = $rdfGraph->primaryTopic();
-        $triples = $rdfGraph->countTriples();
-        $type = $rdfGraph->type();
-        //$rdfGraph->
-        //$t3 = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','rdau:placeOfPublication');
-        $t3 = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','ns0:placeOfPublication');
-        $t = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','rdfs:isDefinedBy');
-        $t1 = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','dct:title');
-        $t2 = $rdfGraph->getLiteral('http://data.swissbib.ch/resource/212817795','dct:title');
-        //$t3 = $rdfGraph->getResource('http://data.swissbib.ch/resource/212817795','rdau:placeOfPublication');
-        //$t = $rdfGraph
-        //todo: evaluate response type - parse error?
+        $rdfGraph->parse(json_encode($this->fields['_source']),'jsonld');
         return $rdfGraph->serialise($rdfType);
-        //$ntriples = $rdfGraph->serialise('ntriples');
-        //$jsonld = $rdfGraph->serialise('jsonld');
 
+    }
+
+
+    public function getRdfType()
+    {
 
     }
 
