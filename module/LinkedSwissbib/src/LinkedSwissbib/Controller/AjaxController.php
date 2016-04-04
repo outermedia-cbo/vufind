d091fe64f0d55124d990f060dbf0931b2b167e47
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2016-04-04 18:46:10 +0200

    Read parameter 'searcher' and act upon it

--- module/LinkedSwissbib/src/LinkedSwissbib/Controller/AjaxController.php
+++ module/LinkedSwissbib/src/LinkedSwissbib/Controller/AjaxController.php
@@ -10,13 +10,17 @@ class AjaxController extends VFAjaxController {
 
     public function jsonAction() {
 
-        // https://arjunphp.com/return-json-response-zend-framework-2/
-        $content = array( 'foo' => 'bar' );
-        $response = $this->getResponse();
-        $response->getHeaders()->addHeaderLine( 'Content-Type', 'application/json' );
-        $response->setContent(json_encode($content));
-        return $response;
+        $searcher = $this->params()->fromQuery('searcher');
 
+        if ($searcher == "Solr") {
+            return parent::jsonAction();
+        } else {
+            // https://arjunphp.com/return-json-response-zend-framework-2/
+            $content = array( 'foo' => 'bar' );
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaderLine( 'Content-Type', 'application/json' );
+            $response->setContent(json_encode($content));
+            return $response;
+        }
     }
-
 }
