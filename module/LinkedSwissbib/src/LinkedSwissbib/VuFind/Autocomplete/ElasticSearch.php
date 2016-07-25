2646da51a10eebc9e4e6b7f0267f6df8ad5cfdfa
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2016-07-25 18:34:57 +0200

    refine() distinguishes between types of entries

--- module/LinkedSwissbib/src/LinkedSwissbib/VuFind/Autocomplete/ElasticSearch.php
+++ module/LinkedSwissbib/src/LinkedSwissbib/VuFind/Autocomplete/ElasticSearch.php
@@ -67,44 +67,52 @@ class ElasticSearch implements AutocompleteInterface{
                 continue; //TODO: we ignore everything except person and DEFAULT (i.e. subjects) for now
             $id = $current['_source']['@id'];
 
-            if (!isset($current['_source']['dbp:birthDate']) && !isset($current['_source']['schema:birthDate']) && !isset($current['_source']['dbp:birthYear'])) {
-                    $birthDate = "?";
-                } elseif (isset($current['_source']['schema:birthDate'])) {
-                    $birthDate = $this->parseDate($current['_source']['schema:birthDate']);
-                } elseif (isset($current['_source']['dbp:birthDate'])) {
-                    $birthDate = $this->parseDate($current['_source']['dbp:birthDate']);
-                } elseif (isset($current['_source']['dbp:birthYear'])) {
-                    $birthDate = $current['_source']['dbp:birthYear'];
+            $displayname = "";
+
+            if ($type == 'person') {
+                if (!isset($current['_source']['dbp:birthDate']) && !isset($current['_source']['schema:birthDate']) && !isset($current['_source']['dbp:birthYear'])) {
+                        $birthDate = "?";
+                    } elseif (isset($current['_source']['schema:birthDate'])) {
+                        $birthDate = $this->parseDate($current['_source']['schema:birthDate']);
+                    } elseif (isset($current['_source']['dbp:birthDate'])) {
+                        $birthDate = $this->parseDate($current['_source']['dbp:birthDate']);
+                    } elseif (isset($current['_source']['dbp:birthYear'])) {
+                        $birthDate = $current['_source']['dbp:birthYear'];
+                }
+
+                if (!isset($current['_source']['dbp:deathDate']) && !isset($current['_source']['schema:deathDate']) && !isset($current['_source']['dbp:deathYear'])) {
+                    $deathDate = "?";
+                } elseif (isset($current['_source']['schema:deathDate'])) {
+                    $deathDate = $this->parseDate($current['_source']['schema:deathDate']);
+                } elseif (isset($current['_source']['dbp:deathDate'])) {
+                    $deathDate = $this->parseDate($current['_source']['dbp:deathDate']);
+                } elseif (isset($current['_source']['dbp:deathYear'])) {
+                    $deathDate = $current['_source']['dbp:deathYear'];
+                }
+
+                if (!empty($birthDate) && !empty($deathDate)) {
+                    $birthAndDeathDates = ' (' . $birthDate . ' - ' . $deathDate. ')';
+                } elseif (!empty($birthDate) && empty($deathDate)) {
+                    $birthAndDeathDates = ' (' . $birthDate . ' - ?)';
+                } elseif (!empty($deathDate) && empty($birthDate)) {
+                    $birthAndDeathDates = ' (? - ' . $deathDate . ')';
+                } else {
+                    $birthAndDeathDates = "?";
+                }
+
+                if(isset($current['_source']['foaf:firstName']) && isset($current['_source']['foaf:lastName']))
+                    $displayname = $current['_source']['foaf:firstName'] . ' ' . $current['_source']['foaf:lastName']. $birthAndDeathDates;
+                elseif(isset($current['_source']['foaf:lastName']))
+                    $displayname =  $current['_source']['foaf:lastName'] . $birthAndDeathDates;
+                elseif(isset($current['_source']['foaf:name']))
+                    $displayname = $current['_source']['foaf:name'] . $birthAndDeathDates;
+                else
+                    $displayname = "?";
             }
 
-            if (!isset($current['_source']['dbp:deathDate']) && !isset($current['_source']['schema:deathDate']) && !isset($current['_source']['dbp:deathYear'])) {
-                $deathDate = "?";
-            } elseif (isset($current['_source']['schema:deathDate'])) {
-                $deathDate = $this->parseDate($current['_source']['schema:deathDate']);
-            } elseif (isset($current['_source']['dbp:deathDate'])) {
-                $deathDate = $this->parseDate($current['_source']['dbp:deathDate']);
-            } elseif (isset($current['_source']['dbp:deathYear'])) {
-                $deathDate = $current['_source']['dbp:deathYear'];
+            if ($type == 'DEFAULT') { // subject
+               $displayname = $current['_source']['http://d-nb_info/standards/elementset/gnd#preferredNameForTheSubjectHeading'][0]['@value'];
             }
-
-            if (!empty($birthDate) && !empty($deathDate)) {
-                $birthAndDeathDates = ' (' . $birthDate . ' - ' . $deathDate. ')';
-            } elseif (!empty($birthDate) && empty($deathDate)) {
-                $birthAndDeathDates = ' (' . $birthDate . ' - ?)';
-            } elseif (!empty($deathDate) && empty($birthDate)) {
-                $birthAndDeathDates = ' (? - ' . $deathDate . ')';
-            } else {
-                $birthAndDeathDates = "?";
-            }
-
-            if(isset($current['_source']['foaf:firstName']) && isset($current['_source']['foaf:lastName']))
-                $displayname = $current['_source']['foaf:firstName'] . ' ' . $current['_source']['foaf:lastName']. $birthAndDeathDates;
-            elseif(isset($current['_source']['foaf:lastName']))
-                $displayname =  $current['_source']['foaf:lastName'] . $birthAndDeathDates;
-            elseif(isset($current['_source']['foaf:name']))
-                $displayname = $current['_source']['foaf:name'] . $birthAndDeathDates;
-            else
-                $displayname = "?";
 
             $results[] = array($id, $type, $displayname);
         }
