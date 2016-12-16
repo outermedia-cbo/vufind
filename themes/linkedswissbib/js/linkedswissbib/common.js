29a969a68f1b75ab8a35f2a01c08389371190306
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2016-12-16 09:49:24 +0100

    [broken] Re-add typeahead library / Replace jQuery UI with variant that doesn't contain Autocomplete widget
    jQuery UI's Autocomplete widget seems to interfere with typeahead, therefore a custom variant of jQuery UI 1.12.1 is used here.

--- themes/linkedswissbib/js/linkedswissbib/common.js
+++ themes/linkedswissbib/js/linkedswissbib/common.js
@@ -1,12 +1,11 @@
-/**
- * Created by thomas on 04.04.16.
- */
-
-$.getScript('../themes/linkedswissbib/js/jquery-ui.min.js', function() { // http://stackoverflow.com/a/18261253
+$.getScript('../themes/linkedswissbib/js/jquery-ui.min.noautocomplete.js', function() { // http://stackoverflow.com/a/18261253
     //script is loaded and executed put your dependent JS here
 });
 
-/*
+$.getScript('../themes/linkedswissbib/js/typeahead.bundle.min.js', function() { // http://stackoverflow.com/a/18261253
+    //script is loaded and executed put your dependent JS here
+});
+
 $(document).ready(function() {
 
     // Search autocomplete
@@ -138,5 +137,3 @@ $(document).ready(function() {
             window.location.href = baseurl + postfix;
         });
 });
-
-*/
