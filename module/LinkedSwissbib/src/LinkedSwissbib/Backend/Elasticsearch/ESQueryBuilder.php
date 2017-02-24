96a7520543d074719846479121bc35d0f7c47cd0
Guenter Hipler <guenter.hipler@unibas.ch>
2017-02-24 11:13:33 +0100

    some changes after hints from Chur

--- module/LinkedSwissbib/src/LinkedSwissbib/Backend/Elasticsearch/ESQueryBuilder.php
+++ module/LinkedSwissbib/src/LinkedSwissbib/Backend/Elasticsearch/ESQueryBuilder.php
@@ -99,14 +99,34 @@ class ESQueryBuilder implements ESQueryBuilderInterface
             $getParams['body'] = '{"index":"gnd","type":"DEFAULT"}'."\n".'{"query":{"filtered":{"filter":{"in":{"_id":["' . implode('","', $uris) . '"]}}}}}'."\n";
             //$getParams['body'] = '{"query":{"filtered":{"filter":{"in":{"_id":["' . implode('","', $uris) . '"]}}}}}'."\n";
             $getParams['type'] = array("DEFAULT"); //TODO: Find out whether this line is necessary
-            //$getParams['index'] = array("gnd");
-            $getParams['index'] = array("lsb");
+            $getParams['index'] = array("gnd");
 
             return $getParams;
         }
 
         $esQuery = new MultisearchQuery($vuFindQuery,$searchHandlerType->getSpec(), $this);
         $searchBody =  $esQuery->build();
+
+        //strip last element from search expression
+        $searchBody=substr($searchBody,0,-2); //remove "}\n" from search expression
+        //Add size to search body
+        $size=$this->params->get('size')[0];
+        if (isset($size) && is_numeric($size))
+        {
+            //add number of results to search expression
+            $searchBody = $searchBody . ',"size": ' . $size;
+        }
+        //Add offset to search body
+        $from=$this->params->get('from')[0];
+        if (isset($size) && is_numeric($size))
+        {
+            //add start index to search expression
+            $searchBody = $searchBody . ',"from": ' . $from;
+        }
+        //finalise search expression
+        $searchBody = $searchBody . "}\n";
+
+
 
         $getParams['body'] = $searchBody;
         $getParams['type'] = array("person", "DEFAULT");
